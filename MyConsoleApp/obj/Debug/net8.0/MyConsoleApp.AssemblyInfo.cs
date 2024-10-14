a8ed0e3502597f94625d8b2825a34a715985ad2d
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2024-10-14 19:25:46 +0300

    init

--- MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d48edad18181cb307df60898321441df9ab9ddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5484ea209e40e066f48dfd98a791e202183cece7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
