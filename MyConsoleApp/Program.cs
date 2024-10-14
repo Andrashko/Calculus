3b523457f2333fa302b351f46ca0ed70980f0dc4
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2024-10-14 10:22:40 +0300

    fix

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -4,7 +4,7 @@ double b = 2 * Math.PI / 3;
 double e = 1e-6;
 
 
-IQuadrature quadrature;
+IQuadrature quadrature = null;
 Console.WriteLine("Виберіть метод інтегрування: \n 1 - Лівих прямокутників \n 2 - Правих прямокутникі \n 3 - Сімпсона");
 int choise = int.Parse(Console.ReadLine());
 switch (choise)
@@ -21,6 +21,6 @@ switch (choise)
 }
 
 NumericalIntegrationMethod method1 = new MontecarloMethod(f);
-NumericalIntegrationMethod method2 = new RungeMethod(f, new SimpsonMethod());
-Console.WriteLine($"Інтеграл функції sin(x) на  [{a};{b}] з точністю {e} рівний {method1.Integrate(a, b, e)}");
-Console.WriteLine($"Інтеграл функції sin(x) на  [{a};{b}] з точністю {Math.Sqrt(e)} рівний {method2.Integrate(a, b, e)}");
+NumericalIntegrationMethod method2 = new RungeMethod(f, quadrature);
+Console.WriteLine($"Інтеграл функції sin(x) методом Монте Карло на  [{a};{b}] з точністю { Math.Sqrt(e)} рівний {method1.Integrate(a, b, e)}");
+Console.WriteLine($"Інтеграл функції sin(x) Методом Рунге на  [{a};{b}] з точністю {e} рівний {method2.Integrate(a, b, e)}");
